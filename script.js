4f3bfc3c6fe310fd17d7b63b4f38c94f818d9710
unknown <ShubhamPal05>
2022-12-19 21:55:23 +0530

    .then() takes only one parameter

--- script.js
+++ script.js
@@ -3,35 +3,49 @@ var heading1 = document.querySelector("#heading1");
 var header = document.querySelector("#header");
 
 // var x = document.querySelector("#coord");
-
-var weatherData;
-var latitude = 27;
-var longitude = 81;
-var container;
-
-function showPosition(position) {
-    latitude = Math.round(position.coords.latitude);
-    longitude = Math.round(position.coords.longitude);
-    console.log(latitude + "   " + longitude);
-}
-navigator.geolocation.getCurrentPosition(showPosition);
-
-
 // fetch("https://api.openweathermap.org/data/2.5/weather?lat=23&lon=81&appid=0db5651271ddadd063ad6732add762c8").then(x => x.json()).then(data => console.log(data.clouds));
 
-function fecthingData(){
-    return new Promise((resolve, reject) =>{
-        let data = fetch("https://api.openweathermap.org/data/2.5/weather?lat="+ latitude + "&lon=" + longitude + "&appid=0db5651271ddadd063ad6732add762c8");
-        resolve(data);
-        reject("rejected url");
-    });
+var weatherData;
+var container;
+
+function mainFunction(){
+
+        return new Promise((resolve, reject) => {
+
+            setTimeout(() => {
+                reject("refresh & allow to Know You Location !");
+            }, 10000);
+
+            // getting location
+            navigator.geolocation.getCurrentPosition( (position) =>{
+                lat = Math.round(position.coords.latitude);
+                lon = Math.round(position.coords.longitude);
+                console.log(lat + "   " + lon);
+
+                resolve(position.coords);
+
+              });
+
+        });
+
 }
 
-fecthingData().then((data)=>{
-    weatherData = data.json();
-    return weatherData;
+// promise handlers only take only one parameter because any function( Promise ) can return only one value
+mainFunction().then((coords) =>{
+    console.log(coords.latitude + "   " + coords.longitude);
+    let data = fetch("https://api.openweathermap.org/data/2.5/weather?lat="+ coords.latitude + "&lon=" + coords.longitude + "&appid=0db5651271ddadd063ad6732add762c8");
+
+    return data;
+
 }).then((data) =>{
 
+    weatherData = data.json();
+
+    return weatherData;
+
+}).then((data) =>{
+
+    console.log("third then", data);
     container = document.createElement("div");
 
     var p1 = document.createElement("p");
@@ -76,50 +90,51 @@ fecthingData().then((data)=>{
 
 
 
+    function renderData(){
+        console.log("I got clicked");
+    
+        console.log(container);
+    
+        fetchDatabtn.style.display= "none";
+    
+        heading1.innerHTML = "Weather API";
+    
+        var HeadingLat = document.createElement("p");
+        HeadingLat.innerText=`latitude: ${lat}`;
+    
+        var HeadingLon = document.createElement("p");
+        HeadingLon.innerText=`longitude: ${lon}`;
+    
+        header.appendChild(HeadingLat);
+        header.appendChild(HeadingLon);
+    
+        var h1 = document.createElement("h1");
+        h1.innerText = "Weather Data";
+    
+        document.body.appendChild(h1);
+        document.body.appendChild(container);
+    }
+
+
+
+    fetchDatabtn.addEventListener("click", renderData);
+
+
 }).catch((err) =>{
-    console.log(err);
+    window.alert(err);
 });
 
-function renderData(){
-    fetchDatabtn.style.display= "none";
 
-    heading1.innerHTML = "Weather API";
 
-    var HeadingLat = document.createElement("p");
-    HeadingLat.innerText=`latitude: ${latitude}`;
-
-    var HeadingLon = document.createElement("p");
-    HeadingLon.innerText=`longitude: ${longitude}`;
-
-    header.appendChild(HeadingLat);
-    header.appendChild(HeadingLon);
-
-    var h1 = document.createElement("h1");
-    h1.innerText = "Weather Data";
-
-    document.body.appendChild(h1);
-    document.body.appendChild(container);
-}
 
 // function renderData() {
 //     fetchDatabtn.style.display = "none";
 //     var elementsCreated = [];
 //     elementsCreated.push(document.createElement("h1").innerText = "Weather Data");
-//     elementsCreated.push(document.createElement("p").innerText = "Location: " + weatherData.name);
-//     elementsCreated.push(document.createElement("p").innerText = "Lat: " + weatherData.coord.lat);
-//     elementsCreated.push(document.createElement("p").innerText = "Long: " + weatherData.coord.lon);
-//     elementsCreated.push(document.createElement("p").innerText = "TimeZone: " + weatherData.timezone);
-//     elementsCreated.push(document.createElement("p").innerText = "Wind Speed: " + weatherData.wind.speed);
-//     elementsCreated.push(document.createElement("p").innerText = "Pressure: " + weatherData.main.pressure);
-//     elementsCreated.push(document.createElement("p").innerText = "Humidity: " + weatherData.main.humidity);
-//     elementsCreated.push(document.createElement("p").innerText = "Wind Direction: " + weatherData.wind.deg);
-//     elementsCreated.push(document.createElement("p").innerText = "UV Index: " + "5.2");
-//     elementsCreated.push(document.createElement("p").innerText = "Feels Like: " + weatherData.main.feels_like);
+//     document.getElementById("latti").innerHTML = `${data.coord.lat}`
 
 //     for(var i=0; i< elementsCreated.length; i++){
 //         document.body.appendChild(elementsCreated[i]);
 //     }
 
 // };
-
-fetchDatabtn.addEventListener("click", renderData);
